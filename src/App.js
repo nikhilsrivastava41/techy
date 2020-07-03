93d407f1ef2830e8dd5b7e2514fbf572ac767255
Nikhil Srivastava <nikhilsrivastava41@gmail.com>
2020-07-03 09:13:52 +0530

    second commmit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Default from "./pages/Default";
+import Contact from "./pages/ContactPage";
+import SingleProduct from "./pages/SingleProductPage";
+import Cart from "./pages/CartPage";
 class App extends Component {
   render() {
-    return <h1>hey</h1>;
+    return (
+      <div>
+        <h1>hey</h1>
+      </div>
+    );
   }
 }
-
 export default App;
