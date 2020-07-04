302c4d299f9388744dcf8a9b3e1182e41d549352
Nikhil Srivastava <nikhilsrivastava41@gmail.com>
2020-07-04 09:31:58 +0530

    Navbar Done

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ProductProvider } from "./context";
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+  <ProductProvider>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </ProductProvider>,
   document.getElementById("root")
 );
 
