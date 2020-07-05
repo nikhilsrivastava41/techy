04baadf0a7b7d30219e7e6cd8cab5312d600309a
Nikhil Srivastava <nikhilsrivastava41@gmail.com>
2020-07-05 09:49:40 +0530

    sidecart hero

--- src/context/context.js
+++ src/context/context.js
@@ -7,8 +7,9 @@ class ProductProvider extends Component {
   state = {
     sidebarOpen: false,
     cartOpen: false,
-    cartItems: 110,
+    cartItems: 2,
     links: linkData,
+    cart: [],
   };
   // handle sidebar
   handleSidebar = () => {
